4744706e974a19d8f587ee2ee8e1bc32dcb809ed
duval2dev <duval_donfack@uni2growcameroun.com>
2026-03-27 16:57:48 +0100

    Refactor code

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,12 @@
+import type { ErrorResponse } from '../api';
 import { authApi } from '../services';
 import { handleUnauthorized } from '../utils/handleUnauthorized';
 
 export const logoutAPI = async () => {
   try {
     await authApi.logout();
-  } catch (error: any) {
-    if (error.status === 401) {
+  } catch (error: unknown) {
+    if ((error as ErrorResponse).status === 401) {
       await handleUnauthorized();
     }
     throw error;
